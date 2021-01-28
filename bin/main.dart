2011af6b54f44780b344e1c2fec0d58d45829ca8
Dev Borland <devborland@gmail.com>
2021-01-28 16:21:13 +0300

    add image coverter

--- bin/main.dart
+++ bin/main.dart
@@ -1,26 +1,31 @@
 import 'package:prompter_sg/prompter_sg.dart';
 import 'dart:io';
-import 'package:image/image.dart';
+import 'package:converter/src/converter.dart';
 
 void main(List<String> args) {
   final prompter = Prompter();
-  final choice = prompter.askBinary('Do you want convert a image?');
+  // final choice = prompter.askBinary('Do you want convert a image?');
 
-  if (!choice) {
-    exit(0);
-  }
+  // if (!choice) {
+  //   exit(0);
+  // }
 
   final format = prompter.askMultiple('Select format:', buildFormatOptions());
   final selectedFile =
       prompter.askMultiple('Select an image to convert', buildFileOptions());
 
-  print(selectedFile);
-  print(format);
+  final newPath = convertImage(selectedFile, format);
+
+  final bool shoulOpen = prompter.askBinary('Open the image?');
+
+  if (shoulOpen) {
+    Process.run('start', ["", '$newPath']);
+  }
 }
 
 List<Option> buildFormatOptions() {
   return [
-    Option('Convert to jpeg', 'jpeg'),
+    Option('Convert to jpeg', 'jpg'),
     Option('Convert to png', 'png'),
   ];
 }
